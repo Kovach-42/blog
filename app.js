51fbf240b6bfbd4a00fdf02d2530485e88b5595d
Kovach-42 <mortonmu8@gmail.com>
2021-08-24 18:49:26 +0200

    remove commented called

--- app.js
+++ app.js
@@ -20,25 +20,6 @@ const blogSchema = new mongoose.Schema({
 // create model
 const Post = mongoose.model('Post', blogSchema);
 
-// create document
-
-// const technology = new Post({
-//     title: 'Technology',
-//     content: "Technology is the answer to humanity's problems.",
-// });
-
-// const food = new Post({
-//     title: 'Food',
-//     content: "Food is needed for the strengthening of one's body.",
-// });
-
-// const health = new Post({
-//     title: 'Health',
-//     content: 'A healthy body is a wealthy body. You need to take care of it.',
-// });
-
-// post.save();
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -74,3 +55,4 @@ app.get('/posts/:postId', (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
